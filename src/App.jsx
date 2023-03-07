f458dc5bb5320dd112603d81610fc16769fd704e
Natalie <73793979+Zukerka@users.noreply.github.com>
2023-03-07 12:02:18 +0200

    button and title components added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Button from './Button';
+import Title from './Title';
+
+const App = () => {
+  return (
+    <div>
+
+      <Title name="Hello"/>
+
+      <Button text="New button" />
+      <Button text="Second button" />
+      <Button text="Third button" />
+      
+    </div>
+  );
+}
+
+export default App;
